e23340e9a8ab7ba874525f47c2f7d0cab89e2adb
Your Name <you@example.com>
2025-04-08 13:18:36 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,290 @@
+export const Bio = {
+  name: "Debashis Boruah",
+  roles: [
+    "Full Stack Developer",
+    // "Android Developer",
+    // "UI/UX Designer",
+    // "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile developer with a passion for full-stack development, always eager to take on new challenges.I thrive under pressure and adapt quickly to changing environments. As a dedicated team member, I contribute effectively to collaborative projects while maintaining a keen focus on delivering high-quality results. I am ready to make meaningful contributions to innovative projects and achieve great things in the tech industry.",
+ 
+  resume:
+    "https://drive.google.com/file/d/1auGppWpgs2chLyOUKgwfoFmq_XTNCDoe/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/debashis-boruah-194210234/",
+  github: "https://github.com/iceynight",
+  insta: "https://www.instagram.com/borruah/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+     {
+    name: "Postgresql",
+    image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+  },
+  {
+    name: "MySQL",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+  },
+  {
+    name: "MongoDB",
+    image:
+      "https://icon.icepanel.io/Technology/svg/MongoDB.svg",
+  },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+     
+      {
+        name: "Spring Boot",
+        image: "https://img.icons8.com/?size=100&id=90519&format=png&color=000000",
+      },
+      {
+        name: "PHP",
+        image:"https://pngimg.com/uploads/php/php_PNG18.png",
+      },
+    ],
+  },
+  {
+    title: "Mobile Development",
+    skills: [
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "Dart",
+        image:
+          "https://img.icons8.com/?size=100&id=7AFcZ2zirX6Y&format=png&color=000000",
+      },
+    ],
+  },
+  
+  
+];
+
+export const experiences = [
+  // {
+  //   id: 0,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+  //   role: "DevOps & Fullstack Engineering Interndjkvjsdcdjcsdjcds",
+  //   company: "Flipr Inovations Pvt. Ltd.",
+  //   date: "Aug 2023 - July 2023",
+  //   desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+  //   skills: [
+  //     "Docker",
+  //     "Terraform",
+  //     "AWS",
+  //     "EC2",
+  //     "Portainer",
+  //     "Nginx",
+  //     "JavaScript",
+  //     "TypeScript",
+  //     "Node Js",
+  //     " Next Js",
+  //   ],
+  //   doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  // },
+  // {
+  //   id: 0,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+  //   role: "Frontend Engineer Intern",
+  //   company: "Neurobit",
+  //   date: "June 2023 - Present",
+  //   desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+  //   skills: [
+  //     "ReactJS",
+  //     "Redux",
+  //     "NodeJs",
+  //     "Material UI",
+  //     "HTML",
+  //     "CSS",
+  //     "JavaScript",
+  //   ],
+  //   doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  // },
+  {
+    id: 1,
+    img: "https://www.uxdt.nic.in/wp-content/uploads/2020/06/nic-logo-nic-logo-1-bilingual-white-1-01.jpg?x76268",
+    role: "Full Stack Developer (Intern)",
+    company: "National Informatics Centre, Meghalaya",
+    date: "Jan 2025 - June 2025",
+    desc: "Responsible for the development and maintenance of the Meghalaya State Commission for Protection of Child Rights website and app, including the creation of comprehensive database systems and administrative dashboards for tracking of child protection cases reported by District Child Protection Units (DCPUs), law enforcement agencies, and judicial bodies throughout Meghalaya.",
+    skills: [
+      "ReactJS",
+      "Flutter",
+      "Dart",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Springboot",
+      "Postgresql",
+    ],
+  },
+  // {
+  //   id: 2,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+  //   role: "Open Source Contributor ",
+  //   company: "GirlScript Summer of Code",
+  //   date: "May 2023 - Present",
+  //   desc: "Contributing to different open-source projects and learn from industry experts",
+  //   doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  // },
+  // {
+  //   id: 3,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+  //   role: "Android Developer",
+  //   company: "DSC KIIT",
+  //   date: "Nov2021 - Present",
+  //   desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+  //   skills: [
+  //     "Leadership",
+  //     "Mobile Application Development",
+  //     "Kotlin",
+  //     "XML",
+  //     "Figma",
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+  //   role: "Android Developer Intern",
+  //   company: "Rudraksha Welffare Foundation",
+  //   date: "June 2021 - Oct 2021",
+  //   desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+  //   skills: [
+  //     "Android",
+  //     "Java",
+  //     "Kotlin",
+  //     "XML",
+  //     "Node Js",
+  //     "Cloud Firestore",
+  //     "Firebase",
+  //     "Figma",
+  //   ],
+  //   doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  // },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.tezu.ernet.in/images/tulogo.png",
+    school: "Tezpur University",
+    date: "Sep 2023 - June 2025",
+    grade: "6.5 CGPA",
+    desc: "I am currently pursuing my Masters degree in Computer Application at Tezpur University. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Master of Computer Applications (MCA)",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/en/7/70/Jorhat_Institute_of_Science_%26_Tech_Logo.png",
+    school: "Jorhat Institute of Science and Technology",
+    date: "Sep 2020 - June 2023",
+    grade: "8.0 CGPA",
+    desc: "I completed my Bachelor of Science in Information Technology from Jorhat Institute of Science and Technology in 2023.",
+    degree: "Bachelor of Science in Information Technology (B.Sc IT)",
+  },
+  {
+    id: 2,
+    img: "https://scontent.fmaa8-1.fna.fbcdn.net/v/t39.30808-1/359486509_609803244610108_1412174998596369035_n.jpg?stp=c0.0.228.228a_dst-jpg_s200x200_tt6&_nc_cat=102&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=F4UgnAe-PusQ7kNvgGiNItj&_nc_oc=AdlCZZCX9BUOUiFabEDRt1jv4yKwO202ly1NwGJBR1P3-LoxuRjJfIdSncIU6X50ImA&_nc_zt=24&_nc_ht=scontent.fmaa8-1.fna&_nc_gid=YkBi87dWzlLKB6wzhOqwXw&oh=00_AYGV0dQJrXwuOjH7ocmJNduo0wFRBFe1buQ73dGApB0chQ&oe=67F4119C",
+    school: "Concept Junior College",
+    date: "May 2017 - Mar 2019",
+    grade: "67%",
+    desc: "I completed my class 12 education at Concept Junior College, Nagaon, Assam, where my stream was Science.",
+    degree: "XI-XII, AHSEC (Science)",
+  },
+  {
+    id: 3,
+    img: "https://play-lh.googleusercontent.com/5trSDlygfnqfI19RF9OrqZoUHKyCJazl555jrxmVnmdMhqvJHHJpzcQaW4l8khODpxY",
+    school: "Christ Jyoti School",
+    date: "Jan 2016 - Jan 2017",
+    grade: "74.5%",
+    desc: "I completed my class 10 education at Christ Jyoti School, Nagaon, Assam.",
+    degree: "X, SEBA",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Cafe Bayleaf",
+    date: "Apr 2022",
+    description: "Developed a comprehensive web-based ordering platform for a café that enables customers to browse menu items and place orders online while providing restaurant staff with order management capabilities. Built using HTML, CSS, JavaScript, PHP, and SQL to create a robust and efficient system.",
+        image:"https://github.com/iceynight/cafebayleaf/blob/main/bayleaf.png?raw=true",
+    tags: ["HTML", "CSS", "Javascript", "Bootstrap", "Php", "MySQL"],
+    category: "web app",
+    github: "https://github.com/iceynight/cafebayleaf",
+    webapp: "https://cafebayleaf.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "EduCare",
+    date: "Jan 2023",
+    description:
+      "Developed a e-learning platform for a coaching center using HTML, CSS & JavaScript. The system enables instructors to upload and manage course videos and educational content, while students can browse, select, and access their desired courses. Implemented a tiered access model where premium content requires payment, alongside free educational resources. Created a responsive interface ensuring seamless user experience across all devices. The platform features secure user authentication, personalized dashboards for both educators and students, and an integrated payment system for premium course purchases.",
+       image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "MongoDb",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  
+  {
+    id: 8,
+    title: "Gesture Controlled Robotic Arm Using Computer Vision",
+    date: "May 2023",
+    description:
+      "A robotic manipulation system that interprets hand gestures captured by an external camera through computer vision techniques. The system employs the hand_recognition library in conjunction with OpenCV to accurately detect and classify various hand gestures. An Arduino microcontroller serves as the processing unit, interpreting the recognized gestures and translating them into precise mechanical movements of the robotic arm.",
+    image:
+      "https://lh3.googleusercontent.com/proxy/YiwUa0zChb_pk7Qc-6NYbjzLJbaig-n5vrsO0ywhsbRBAxUmS9gftP2-08sW8fvag5KMnMzw8XFBarUVVPPYi52MlNZ_2CkmFpY",
+    tags: ["Python", "Arduino", "OpenCV", "CV Zone"],
+    category: "machine learning",
+    github: "https://github.com/iceynight/Gesture-Controlled-Robotic-Arm",
+    webapp: "https://github.com/iceynight/Gesture-Controlled-Robotic-Arm",
+  },
+];
